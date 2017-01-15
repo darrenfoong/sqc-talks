cbc94f6a81a5f3d029129b0d3615e0a5817a6d39
Darren Foong <darrenfoong@gmail.com>
2017-01-15 18:27:39 +0800

    Improve fact code

--- algs-ds/fact.hs
+++ algs-ds/fact.hs
@@ -1,12 +1,11 @@
 module Main where
 
 fact 0 = 1
-fact 1 = 1
 fact n = n * fact (n-1) -- not tail-recursive
 
 fact2 n = fact2inner n 1
 
-fact2inner n acc = if n > 1
+fact2inner n acc = if n > 0
                    then fact2inner (n-1) (n*acc)
                    else acc -- tail-recursive
 
